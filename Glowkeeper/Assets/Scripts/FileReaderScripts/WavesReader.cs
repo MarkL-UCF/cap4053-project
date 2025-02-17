e649e926164b7acd7d7387ae70e0e1a7ea569d6e
MarkL-UCF <ma999600@ucf.edu>
2025-02-17 15:56:25 -0500

    Created the Wave Atlas System

--- Glowkeeper/Assets/Scripts/FileReaderScripts/WavesReader.cs
+++ Glowkeeper/Assets/Scripts/FileReaderScripts/WavesReader.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 
 public class WavesReader : MonoBehaviour
 {
+    WaveAtlas atlas;
+
     // Start is called before the first frame update
     void Start()
     {
+        atlas = GameObject.FindGameObjectWithTag("Atlas (Wave)").GetComponent<WaveAtlas>();
         ReadCSV();
     }
 
@@ -28,7 +31,40 @@ public class WavesReader : MonoBehaviour
             var vals = line.Split(',');
             //get data here
 
-            Debug.Log("value read: " + vals[0]);
+            //format of waves.csv
+
+            //cell A: Floor where wave can occur
+            //cells B-G: Wave 1 Enemy IDs
+            //cells H-M: Wave 2 Enemy IDs
+            //cells N-S: Wave 3 Enemy IDs
+
+            //Reminder that an ID of 0 means no enemy will spawn
+
+            if (int.Parse(vals[0]) == 1) //Floor one wave
+            {
+                atlas.f1w1s1.Add(int.Parse(vals[1]));
+                atlas.f1w1s2.Add(int.Parse(vals[2]));
+                atlas.f1w1s3.Add(int.Parse(vals[3]));
+                atlas.f1w1s4.Add(int.Parse(vals[4]));
+                atlas.f1w1s5.Add(int.Parse(vals[5]));
+                atlas.f1w1s6.Add(int.Parse(vals[6]));
+
+                atlas.f1w2s1.Add(int.Parse(vals[7]));
+                atlas.f1w2s2.Add(int.Parse(vals[8]));
+                atlas.f1w2s3.Add(int.Parse(vals[9]));
+                atlas.f1w2s4.Add(int.Parse(vals[10]));
+                atlas.f1w2s5.Add(int.Parse(vals[11]));
+                atlas.f1w2s6.Add(int.Parse(vals[12]));
+
+                atlas.f1w3s1.Add(int.Parse(vals[13]));
+                atlas.f1w3s2.Add(int.Parse(vals[14]));
+                atlas.f1w3s3.Add(int.Parse(vals[15]));
+                atlas.f1w3s4.Add(int.Parse(vals[16]));
+                atlas.f1w3s5.Add(int.Parse(vals[17]));
+                atlas.f1w3s6.Add(int.Parse(vals[18]));
+
+                atlas.f1PoolSize++;
+            }
         }
     }
 }
