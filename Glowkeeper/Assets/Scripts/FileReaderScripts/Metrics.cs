bf991405236496785d7e1accbca189721420b501
MarkL-UCF <ma999600@ucf.edu>
2025-03-07 11:33:27 -0500

    Starting metrics script

---
+++ Glowkeeper/Assets/Scripts/FileReaderScripts/Metrics.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Metrics : MonoBehaviour
+{
+    private float creationTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        creationTime = Time.time;
+
+    }
+
+    public void startRun()
+    {
+
+    }
+}
