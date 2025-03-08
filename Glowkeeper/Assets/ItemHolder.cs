202515d739f3229dff06ac63081cbc5814e62dd0
Taylor Shook <123588870+TaylorJShook@users.noreply.github.com>
2025-03-08 14:57:22 -0500

    Items Now Stack
    Items stack, the is a cap of 10 at the moment that can be adjusted. There cannot be duplicates.

--- Glowkeeper/Assets/ItemHolder.cs
+++ Glowkeeper/Assets/ItemHolder.cs
@@ -8,34 +8,48 @@ public class ItemHolder : MonoBehaviour
     
     public PlayerItems CurrentItem;
     public PlayerItems[] Items;
+    int numItems;
     public Boolean newPickup;
 
     private void Start()
     {
         newPickup = false;
+        Items = new PlayerItems[10];
+        numItems = 0;
     }
     // Update is called once per frame
     void Update()
     {
         if (newPickup)
         {
-            int i = 0;
             CurrentItem.Info();
 
-            
-            for (i = 0; i < Items.Length; i++)
-            {
-                if (CurrentItem.Name == Items[i].Name)
-                {
-                    newPickup = false;
-                }
-                else if (i == Items.Length - 1)
-                {
-                    Items[i + 1] = CurrentItem;
-                    CurrentItem.Activate(gameObject);
-                    newPickup = false;
-                }
 
+            if (numItems == 0)
+            {
+                Items[0] = CurrentItem;
+                CurrentItem.Activate(gameObject);
+                newPickup = false;
+                numItems = 1;
+            }
+            else
+            {
+                for (int i = 0; i < numItems; i++)
+                {
+                    if (CurrentItem.name == Items[i].name)
+                    {
+                        newPickup = false;
+                        return;
+                    }
+                    else if (i == numItems - 1)
+                    {
+                        Items[i+1] = CurrentItem;
+                        CurrentItem.Activate(gameObject);
+                        newPickup = false;
+                        numItems++;
+                    }
+
+                }
             }
         }
     }
