3ac920d149a1ecba388da2ecce1c5d0157657110
MarkL-UCF <ma999600@ucf.edu>
2025-03-08 19:22:24 -0500

    Converted Rare Candy's handling to be counted as a pickup rather than an item

--- Glowkeeper/Assets/Scripts/ItemScripts/CandyItem.cs
+++ Glowkeeper/Assets/Scripts/ItemScripts/CandyItem.cs
@@ -2,38 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu]
-public class CandyItem : PlayerItems
+
+public class CandyItem : MonoBehaviour
 {
-    //Precise!
-    public override void Activate(GameObject parent)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        var Weapon = GameObject.FindGameObjectWithTag("Weapon").GetComponent<PlayerWeapon>();
-
-        int val = Random.Range(1, 5);
-
-        switch (val)
+        if (collision.gameObject.CompareTag("Player"))
         {
-            case 1:
-                Weapon.damageFlat += 0.25f;
-                break;
+            var Weapon = GameObject.FindGameObjectWithTag("Weapon").GetComponent<PlayerWeapon>();
 
-            case 2:
-                Weapon.firerateFlat += 0.20f;
-                break;
+            int val = Random.Range(1, 5);
 
-            case 3:
-                Weapon.projectileSpeedFlat += 0.20f;
-                break;
+            switch (val)
+            {
+                case 1:
+                    Weapon.damageFlat += 0.25f;
+                    break;
 
-            case 4:
-                Weapon.spreadFlat -= 1;
-                break;
+                case 2:
+                    Weapon.firerateFlat -= 0.20f;
+                    break;
 
-            case 5:
-                Weapon.projectileSizeFlat += .1f;
-                break;
+                case 3:
+                    Weapon.projectileSpeedFlat += 0.20f;
+                    break;
+
+                case 4:
+                    Weapon.spreadFlat -= 1;
+                    break;
+
+                case 5:
+                    Weapon.projectileSizeFlat += .1f;
+                    break;
+            }
+
+            Destroy(gameObject);
         }
-
     }
 }
