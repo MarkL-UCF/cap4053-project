e3af1e1982a10d2cf180d942e0132ff718e9526f
MarkL-UCF <ma999600@ucf.edu>
2025-04-17 12:02:44 -0400

    Updated Candy to support purchasing

--- Glowkeeper/Assets/Scripts/ItemScripts/CandyItem.cs
+++ Glowkeeper/Assets/Scripts/ItemScripts/CandyItem.cs
@@ -1,17 +1,45 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 
 public class CandyItem : MonoBehaviour
 {
+    public Boolean shopItem;
+    public Boolean canBuy;
+    public Boolean canPickup;
+    public int cost = 0;
+    public TextMeshProUGUI priceText;
+    public TextMeshProUGUI pickUpText;
+
+    private void Start()
+    {
+        canBuy = false;
+        canPickup = false;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && canBuy && canPickup)
+        {
+            playerCurrency currency = GameObject.FindGameObjectWithTag("Player").GetComponent<playerCurrency>();
+            currency.SpendCoins(cost);
+            playerHealth Health = GameObject.FindGameObjectWithTag("Player").GetComponent<playerHealth>();
+           
+
+            Destroy(gameObject);
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Player") && !shopItem)
         {
             var Weapon = GameObject.FindGameObjectWithTag("Weapon").GetComponent<PlayerWeapon>();
 
-            int val = Random.Range(1, 5);
+            int val = UnityEngine.Random.Range(1, 5);
 
             switch (val)
             {
@@ -37,6 +65,30 @@ public class CandyItem : MonoBehaviour
             }
 
             Destroy(gameObject);
+        }
+        else if (collision.gameObject.CompareTag("Player") && shopItem)
+        {
+            canPickup = true;
+            playerCurrency currency = GameObject.FindGameObjectWithTag("Player").GetComponent<playerCurrency>();
+            pickUpText = GameObject.Find("PickUp").GetComponent<TextMeshProUGUI>();
+            priceText = GameObject.Find("PriceDisplay").GetComponent<TextMeshProUGUI>();
+            pickUpText.text = "Press 'E' to purchase";
+
+            if (currency.CheckCoins(cost))
+            {
+                canBuy = canPickup;
+                priceText.faceColor = Color.green;
+                priceText.text = "Buy for " + cost;
+
+            }
+            else
+            {
+                priceText.faceColor = Color.red;
+                priceText.text = "Buy for " + cost + ":<br>" + "INSUFFICIENT FUNDS";
+
+            }
+
+
         }
     }
 }
