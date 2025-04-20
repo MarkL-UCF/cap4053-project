84b3336b06054870591e1ccca1b0886fe7732237
squallcoder <tammapavan38@gmail.com>
2025-04-20 15:27:00 -0400

    Fixed Camera Offset Issue

--- Glowkeeper/Assets/Scripts/EnvironmentScripts/DoorTeleporter.cs
+++ Glowkeeper/Assets/Scripts/EnvironmentScripts/DoorTeleporter.cs
@@ -1,92 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-/*
-public class DoorTeleporter : MonoBehaviour
-{
-    
-    public GameObject connectedTeleportSpot;
-    public GameObject MainCamera;
-    public GameObject CameraAnchor;
-    public GameObject itemSpawner;
-    public GameObject ItemAnchor;
 
-    private bool playerInTrigger = false;
-    private GameObject player;
-
-    void Start()
-    {
-        MainCamera = GameObject.FindGameObjectWithTag("MainCamera");
-        itemSpawner = GameObject.FindGameObjectWithTag("Item Spawner");
-    }
-
-    void Update()
-    {
-        if (playerInTrigger && Input.GetKeyDown(KeyCode.E))
-        {
-            TeleportPlayer();
-        }
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.CompareTag("Player"))
-        {
-            Debug.Log($"[DoorTeleporter] Player entered door: {gameObject.name}");
-            playerInTrigger = true;
-            player = collision.gameObject;
-        }
-    }
-
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.CompareTag("Player"))
-        {
-            playerInTrigger = false;
-            player = null;
-        }
-    }
-
-    private void TeleportPlayer()
-    {
-        if (player == null || connectedTeleportSpot == null)
-        {
-            Debug.LogError($"[DoorTeleporter] Teleport failed! Player or connected spot is missing.");
-            return;
-        }
-
-        // Teleport player
-        player.transform.SetPositionAndRotation(connectedTeleportSpot.transform.position, connectedTeleportSpot.transform.rotation);
-        Debug.Log($"[DoorTeleporter] Teleported player to: {connectedTeleportSpot.name}");
-
-        // Move Item Spawner
-        if (itemSpawner != null && ItemAnchor != null)
-        {
-            itemSpawner.GetComponent<ItemSpawner>().DespawnItem();
-            itemSpawner.transform.SetPositionAndRotation(ItemAnchor.transform.position, ItemAnchor.transform.rotation);
-            Debug.Log($"[DoorTeleporter] ItemSpawner moved to: {ItemAnchor.name}");
-        }
-        else
-        {
-            Debug.LogError("[DoorTeleporter] ItemSpawner or ItemAnchor is missing!");
-        }
-
-        // Move Camera
-        if (MainCamera != null && CameraAnchor != null)
-        {
-            MainCamera.transform.SetPositionAndRotation(CameraAnchor.transform.position, CameraAnchor.transform.rotation);
-            Debug.Log($"[DoorTeleporter] Camera moved to: {CameraAnchor.name}");
-        }
-        else
-        {
-            Debug.LogError("[DoorTeleporter] MainCamera or CameraAnchor is missing!");
-        }
-    }
-}
-    */
-
-
-    //Old unbroken code
 public class DoorTeleporter : MonoBehaviour
 {
     public GameObject connectedTeleportSpot;
@@ -115,9 +30,9 @@ public class DoorTeleporter : MonoBehaviour
         if (collision.CompareTag("Player"))
         {
             Vector3 anchorPos = CameraAnchor.transform.position;
-
+            float yOffset = 0.25f;
             // For 2D: use anchor's x and y, but force z = -10 (or your default cam z)
-            Vector3 targetPos = new Vector3(anchorPos.x, anchorPos.y, -10f);
+            Vector3 targetPos = new Vector3(anchorPos.x, anchorPos.y + yOffset , -10f);
             MainCamera.transform.position = targetPos;
 
             collision.gameObject.transform.SetPositionAndRotation(connectedTeleportSpot.transform.position, collision.gameObject.transform.rotation); //teleport player
